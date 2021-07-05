33e867a17486582a694d5fad16df5f74bf952375
Anna Golosova <golosowa.anna@gmail.com>
2021-07-05 15:05:11 +0300

    Implement get all asset summaries and rent group summaries use cases

--- FinancialSummaryApi.Tests/V1/UseCase/GetAllAssetSummariesUseCaseTests.cs
+++ FinancialSummaryApi.Tests/V1/UseCase/GetAllAssetSummariesUseCaseTests.cs
@@ -9,7 +9,6 @@ using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace FinancialSummaryApi.Tests.V1.UseCase
@@ -18,19 +17,16 @@ namespace FinancialSummaryApi.Tests.V1.UseCase
     {
         private Mock<IFinanceSummaryGateway> _mockFinanceGateway;
         private GetAllAssetSummariesUseCase _getAllAssetSummariesUseCase;
-        private Fixture _fixture;
 
         [SetUp]
         public void SetUp()
         {
             _mockFinanceGateway = new Mock<IFinanceSummaryGateway>();
-
             _getAllAssetSummariesUseCase = new GetAllAssetSummariesUseCase(_mockFinanceGateway.Object);
-            _fixture = new Fixture();
         }
 
         [Test]
-        public async Task GetAllAssetSummariesFromTheGatewayWithDate()
+        public async Task GetAll_WithCustomDate_ReturnsListOfAssetSummaries()
         {
             var assetSummaries = new List<AssetSummary>()
             {
@@ -51,7 +47,7 @@ namespace FinancialSummaryApi.Tests.V1.UseCase
                     Id = new Guid("5a0127f7-a9e5-4a50-aff6-d8f9eccc1fd3"),
                     TargetId = new Guid("4f2fb565-84c5-4c8a-9ada-0f03ecd26f45"),
                     TargetType = TargetType.Estate,
-                    SubmitDate = new DateTime(2021, 7, 3),
+                    SubmitDate = new DateTime(2021, 7, 2),
                     AssetName = "Estate 2",
                     TotalDwellingRent = 100,
                     TotalNonDwellingRent = 102,
@@ -62,26 +58,61 @@ namespace FinancialSummaryApi.Tests.V1.UseCase
 
             _mockFinanceGateway.Setup(x => x.GetAllAssetSummaryAsync(It.IsAny<DateTime>())).ReturnsAsync(assetSummaries);
 
-            var response = new List<AssetSummaryResponse>(assetSummaries.ToResponse());
+            var expectedResult = new List<AssetSummaryResponse>(assetSummaries.ToResponse());
 
             var result = await _getAllAssetSummariesUseCase.ExecuteAsync(new DateTime(2021, 7, 2)).ConfigureAwait(false);
 
-            result.Should().HaveCount(1);
+            result.Should().HaveCount(2);
 
-            result.Should().BeEquivalentTo(response);
+            assetSummaries[0].SubmitDate.Should().Be(new DateTime(2021, 7, 2));
+            assetSummaries[1].SubmitDate.Should().Be(new DateTime(2021, 7, 2));
+
+            result.Should().BeEquivalentTo(expectedResult);
         }
 
         [Test]
-        public async Task GetAllAssetSummariesFromTheGatewayWithDefaultDate()
+        public async Task GetAll_WithDefaultDate_ReturnsListOfAssetSummaries()
         {
-            var assetSummaries = _fixture.CreateMany<AssetSummary>().ToList();
+            var assetSummaries = new List<AssetSummary>()
+            {
+                new AssetSummary
+                {
+                    Id = new Guid("4e1a96e2-1a51-4e3e-9c13-29ae4fa90bdc"),
+                    TargetId = new Guid("00a621fa-c951-43a9-ac4b-9023bb6b97e5"),
+                    TargetType = TargetType.Estate,
+                    SubmitDate = DateTime.UtcNow.Date,
+                    AssetName = "Estate 1",
+                    TotalDwellingRent = 100,
+                    TotalNonDwellingRent = 102,
+                    TotalRentalServiceCharge = 20,
+                    TotalServiceCharges = 107
+                },
+                new AssetSummary
+                {
+                    Id = new Guid("5a0127f7-a9e5-4a50-aff6-d8f9eccc1fd3"),
+                    TargetId = new Guid("4f2fb565-84c5-4c8a-9ada-0f03ecd26f45"),
+                    TargetType = TargetType.Estate,
+                    SubmitDate = DateTime.UtcNow.Date,
+                    AssetName = "Estate 2",
+                    TotalDwellingRent = 100,
+                    TotalNonDwellingRent = 102,
+                    TotalRentalServiceCharge = 20,
+                    TotalServiceCharges = 107
+                }
+            };
+
             _mockFinanceGateway.Setup(x => x.GetAllAssetSummaryAsync(It.IsAny<DateTime>())).ReturnsAsync(assetSummaries);
 
-            var response = new List<AssetSummaryResponse>(assetSummaries.ToResponse());
+            var expectedResult = new List<AssetSummaryResponse>(assetSummaries.ToResponse());
 
             var result = await _getAllAssetSummariesUseCase.ExecuteAsync(new DateTime()).ConfigureAwait(false);
 
-            result.Should().BeEquivalentTo(response);
+            result.Should().HaveCount(2);
+
+            assetSummaries[0].SubmitDate.Should().Be(DateTime.UtcNow.Date);
+            assetSummaries[1].SubmitDate.Should().Be(DateTime.UtcNow.Date);
+
+            result.Should().BeEquivalentTo(expectedResult);
         }
     }
 }
