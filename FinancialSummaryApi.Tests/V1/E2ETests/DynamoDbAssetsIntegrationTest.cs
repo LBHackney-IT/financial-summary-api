8ab778d0e063920fbf8ab630379a05d7ac91f13b
golosowa.anna <golosowa.anna@gmail.com>
2021-08-03 14:09:25 +0300

    Modify .circleci/config.yml to remove all terraform actions. Change collection name for e2e tests

--- FinancialSummaryApi.Tests/V1/E2ETests/DynamoDbAssetsIntegrationTest.cs
+++ FinancialSummaryApi.Tests/V1/E2ETests/DynamoDbAssetsIntegrationTest.cs
@@ -17,7 +17,10 @@ using Xunit;
 
 namespace FinancialSummaryApi.Tests.V1.E2ETests
 {
-    [Collection("Collection1")]
+    // Hanna Holasava, [03.08.2021]
+    // We need to specify a collection for unit tests to prevent parallelism for E2E tests
+    // https://xunit.net/docs/running-tests-in-parallel
+    [Collection("MainCollection")]
     public class DynamoDbAssetsIntegrationTest : DynamoDbIntegrationTests<Startup>
     {
         private readonly Fixture _fixture = new Fixture();
