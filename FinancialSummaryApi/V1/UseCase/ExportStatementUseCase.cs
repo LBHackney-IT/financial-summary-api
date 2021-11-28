fb75c7e7efa48b3a5498c9a928d93c2d42f8675f
eddygitgub <87383463+EdwardAnoghena@users.noreply.github.com>
2021-11-28 13:34:56 +0100

    using Rotativa directly

--- FinancialSummaryApi/V1/UseCase/ExportStatementUseCase.cs
+++ FinancialSummaryApi/V1/UseCase/ExportStatementUseCase.cs
@@ -1,27 +1,19 @@
-
 using FinancialSummaryApi.V1.Boundary.Request;
 using FinancialSummaryApi.V1.Domain;
 using FinancialSummaryApi.V1.Gateways.Abstracts;
 using FinancialSummaryApi.V1.UseCase.Helpers;
 using FinancialSummaryApi.V1.UseCase.Interfaces;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
-using Wkhtmltopdf.NetCore;
 
 namespace FinancialSummaryApi.V1.UseCase
 {
     public class ExportStatementUseCase : IExportStatementUseCase
     {
         private readonly IFinanceSummaryGateway _financeSummaryGateway;
-        //private readonly PdfGenerator _pdfGenerator;
-        private readonly IGeneratePdf _generatePdf;
-        private readonly ILogger<ExportStatementUseCase> _logger;
-        public ExportStatementUseCase(IFinanceSummaryGateway financeSummaryGateway, IGeneratePdf generatePdf, ILogger<ExportStatementUseCase> logger)
+        public ExportStatementUseCase(IFinanceSummaryGateway financeSummaryGateway)
         {
             _financeSummaryGateway = financeSummaryGateway;
-            _generatePdf = generatePdf;
-            _logger = logger;
         }
 
         public async Task<byte[]> ExecuteAsync(ExportStatementRequest request)
@@ -47,14 +39,13 @@ namespace FinancialSummaryApi.V1.UseCase
 
             var response = await _financeSummaryGateway.GetStatementListAsync(request.TargetId, startDate, endDate).ConfigureAwait(false);
 
-            var html = TemplateGenerator.GetHTMLReportString1();
+
             var result = request?.FileType switch
             {
                 "csv" => FileGenerator.WriteCSVFile(response, name, period),
-                "pdf" => _generatePdf.GetPDF(html),//FileGenerator.WritePdfFile(response, name, period),
+                "pdf" => null,//FileGenerator.WritePdfFile(response, name, period, _converter),
                 _ => null
             };
-            _logger.LogInformation("File successfully geneated");
             return result;
         }
     }
