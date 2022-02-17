e2c2a381863ed26d26e60d7372c5cf2cd9fe4d40
mdrizy96 <duncanokeno@gmail.com>
2022-02-17 11:02:32 +0300

    Return values ordered by summary year

--- FinancialSummaryApi/V1/UseCase/GetAllAssetSummariesUseCase.cs
+++ FinancialSummaryApi/V1/UseCase/GetAllAssetSummariesUseCase.cs
@@ -4,6 +4,7 @@ using FinancialSummaryApi.V1.Gateways.Abstracts;
 using FinancialSummaryApi.V1.UseCase.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FinancialSummaryApi.V1.UseCase
@@ -22,7 +23,7 @@ namespace FinancialSummaryApi.V1.UseCase
         {
             var assetSummaries = (await _financeSummaryGateway.GetAllAssetSummaryAsync(assetId, null).ConfigureAwait(false)).ToViewResponse();
 
-            return assetSummaries;
+            return assetSummaries.OrderBy(a => a.SummaryYear).ToList();
         }
     }
 }
