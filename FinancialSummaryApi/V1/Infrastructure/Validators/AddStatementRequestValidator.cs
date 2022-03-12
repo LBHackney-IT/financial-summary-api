d815d3654c1e83b9d42bf4669e35487efdf47223
golosowa.anna <golosowa.anna@gmail.com>
2022-03-12 16:09:42 +0300

    Remove implicit validation

--- FinancialSummaryApi/V1/Infrastructure/Validators/AddStatementRequestValidator.cs
+++ FinancialSummaryApi/V1/Infrastructure/Validators/AddStatementRequestValidator.cs
@@ -12,10 +12,6 @@ namespace FinancialSummaryApi.V1.Infrastructure.Validators
             RuleFor(x => x.StatementPeriodEndDate).NotEmpty();
             RuleFor(x => x.RentAccountNumber).NotEmpty();
             RuleFor(x => x.Address).NotEmpty();
-            RuleFor(x => x.StatementType).IsInEnum();
-            RuleFor(x => x.ChargedAmount).GreaterThanOrEqualTo(0);
-            RuleFor(x => x.PaidAmount).GreaterThanOrEqualTo(0);
-            RuleFor(x => x.HousingBenefitAmount).GreaterThanOrEqualTo(0);
         }
     }
 }
