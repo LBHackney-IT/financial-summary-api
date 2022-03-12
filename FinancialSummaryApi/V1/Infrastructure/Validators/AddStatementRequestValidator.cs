427349cb6ce3a034ad463eaa18a4dd525ad01dbf
golosowa.anna <golosowa.anna@gmail.com>
2022-03-12 16:20:53 +0300

    Fix unit tests

--- FinancialSummaryApi/V1/Infrastructure/Validators/AddStatementRequestValidator.cs
+++ FinancialSummaryApi/V1/Infrastructure/Validators/AddStatementRequestValidator.cs
@@ -9,6 +9,7 @@ namespace FinancialSummaryApi.V1.Infrastructure.Validators
         {
             RuleFor(x => x.TargetId).NotEmpty();
             RuleFor(x => x.TargetType).IsInEnum();
+            RuleFor(x => x.StatementType).IsInEnum();
             RuleFor(x => x.StatementPeriodEndDate).NotEmpty();
             RuleFor(x => x.RentAccountNumber).NotEmpty();
             RuleFor(x => x.Address).NotEmpty();
